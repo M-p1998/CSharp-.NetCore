3fa9114f31495a63b9bac903809b3485316ffa20
Mya <skyusmpp@gmail.com>
2024-03-04 11:54:16 -0800

    test

---
+++ Basic13/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+namespace Basic13;
+{
+    public class Program
+    {
+        // public static void PrintNumbers(){
+        //     for(int i=1; i<=255; i++){
+        //         Console.WriteLine(i + " ")
+        //     }
+        //     Console.WriteLine("hi");
+            
+        // }
+        public static void Main(string[] args){
+            // PrintNumbers();
+            Console.WriteLine("hi");
+            
+        }
+    }
+}
